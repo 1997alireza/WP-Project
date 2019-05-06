8e1d37ad098159da48e176c539d8808e24ec00fc
1997alireza <torabian.alireza@gmail.com>
2019-05-06 23:38:17 +0430

    combine login and register page

---
+++ js/authentication.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('#auth-login').click(function () {
+        if($(this)[0].classList.contains('auth-current-section'))
+            return;
+        $('#auth-login').addClass('auth-current-section');
+        $('#auth-reg').removeClass('auth-current-section');
+        $('.display-on-reg').fadeOut(200, function(){
+            $('.display-on-login').fadeIn(200);
+        });
+    });
+
+    $('#auth-reg').click(function () {
+        if($(this)[0].classList.contains('auth-current-section'))
+            return;
+        $('#auth-reg').addClass('auth-current-section');
+        $('#auth-login').removeClass('auth-current-section');
+        $('.display-on-login').fadeOut(200, function(){
+            $('.display-on-reg').fadeIn(200);
+        });
+    });
+});
